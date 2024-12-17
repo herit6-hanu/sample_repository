f5759ce9b5a16c698d5ddf3b814397a49baa61a4
Hanumantha Reddy <hhanumanthareddym2000@gmail.com>
2024-12-17 23:21:24 +0530

    fdsg

---
+++ sample_maven_project/src/test/java/sample_package/SampleClass.java
@@ -0,0 +1,11 @@
+package sample_package;
+
+public class SampleClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Sample Execution....");
+
+	}
+
+}
